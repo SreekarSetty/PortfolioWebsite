070ccfab2028c3610c347c329eb0d3a5e8a71c34
SreekarSetty <156240699+SreekarSetty@users.noreply.github.com>
2026-07-07 10:18:06 -0500

    Initial Commit

---
+++ JavaFSD_PortfolioWebsite/src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent {
+
+  menuOpen = false;
+
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
+
+}
